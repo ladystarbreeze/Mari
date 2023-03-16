bbc4129f7bdea89e8032f714a55b6813d405f960
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2023-03-16 15:13:27 +0100

    Add basic SDL2 rendering

--- src/core/Mari.cpp
+++ src/core/Mari.cpp
@@ -16,13 +16,36 @@
 #include "dmac/dmac.hpp"
 #include "gpu/gpu.hpp"
 #include "timer/timer.hpp"
-#include "../common/types.hpp"
+
+#include <SDL2/SDL.h>
+
+#undef main
 
 namespace ps {
 
 /* --- Mari constants --- */
 
 constexpr i64 RUN_CYCLES = 64;
+
+/* SDL2 */
+SDL_Renderer *renderer;
+SDL_Window *window;
+SDL_Texture *texture;
+SDL_Event *e;
+
+/* Initializes SDL */
+void initSDL() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+
+    SDL_CreateWindowAndRenderer(1024, 512, 0, &window, &renderer);
+    SDL_SetWindowSize(window, 1024, 512);
+    SDL_RenderSetLogicalSize(renderer, 1024, 512);
+    SDL_SetWindowResizable(window, SDL_FALSE);
+    SDL_SetWindowTitle(window, "Mari");
+
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XBGR1555, SDL_TEXTUREACCESS_STREAMING, 1024, 512);
+}
 
 void init(const char *biosPath, const char *isoPath) {
     std::printf("BIOS path: \"%s\"\nISO path: \"%s\"\n", biosPath, isoPath);
@@ -34,6 +57,8 @@ void init(const char *biosPath, const char *isoPath) {
     dmac::init();
     gpu::init();
     timer::init();
+
+    initSDL();
 }
 
 void run() {
@@ -44,6 +69,14 @@ void run() {
 
         scheduler::processEvents(RUN_CYCLES);
     }
+}
+
+void update(const u8 *fb) {
+    SDL_PollEvent(e); // Change this when we implement controllers
+
+    SDL_UpdateTexture(texture, nullptr, fb, 2 * 1024);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
 
 }
--- src/core/Mari.hpp
+++ src/core/Mari.hpp
@@ -5,9 +5,13 @@
 
 #pragma once
 
+#include "../common/types.hpp"
+
 namespace ps {
 
 void init(const char *biosPath, const char *isoPath);
 void run();
+
+void update(const u8 *fb);
 
 }
